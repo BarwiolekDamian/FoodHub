ef93c742175c16e8b8d0e5c2f54784b15347afaa
BarwiolekDamian <damian.barwiolek@student.pk.edu.pl>
2024-05-12 01:10:23 +0200

    feat: added project structure

---
+++ src/test/java/FOODHUB/FoodHubApplicationTests.java
@@ -0,0 +1,11 @@
+package FOODHUB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHubApplicationTests
+{
+	@Test
+	void contextLoads() {}
+}
