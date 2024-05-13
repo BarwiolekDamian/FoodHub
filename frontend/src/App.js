8af90a02f8e0f006c566a8c561e13167ce772d1d
BarwiolekDamian <damian.barwiolek@student.pk.edu.pl>
2024-05-14 00:47:15 +0200

    feat: added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import Error from './views/Error'
+import Login from './views/Login'
+import Register from './views/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App()
 {
 	return (
-		<div></div>
+		<Router>
+			<Routes>
+				<Route path = "/" element = { <Login/> } />
+				<Route path = "/error" element = { <Error/> } />
+				<Route path = "/login" element = { <Login/> } />
+				<Route path = "/register" element = { <Register/> } />
+			</Routes>
+		</Router>
 	);
 }
 
---
+++ frontend/src/views/Register.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Register.css'
+
+const Register = () =>
+{
+    return (
+        <div>
+            <h1>PAGE - Register</h1>
+        </div>
+    );
+}
+
+export default Register;
