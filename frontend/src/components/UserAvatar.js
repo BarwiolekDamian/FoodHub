0e7890f49898dc15626bd9e7b665e732539d7b63
BarwiolekDamian <damian.barwiolek@student.pk.edu.pl>
2024-06-03 00:17:08 +0200

    fix: fixed user avatar display without profile photo

--- frontend/src/components/UserAvatar.js
+++ frontend/src/components/UserAvatar.js
@@ -6,7 +6,7 @@ const UserAvatar = ({ fullName, imageSrc, userLogin }) =>
 {
     return (
         <div className = 'divUserAvatar'>
-            <img className = 'imgUserAvatar-UserImage' src = { imageSrc ? `http://localhost:8080/${imageSrc}` : '' } />
+            <img className = 'imgUserAvatar-UserImage' src = { imageSrc ? `http://localhost:8080/${imageSrc}` : 'default-avatar.png' } />
 
             <h3 className = 'h3UserAvatar-FullName'>{ fullName ? `${fullName}` : '' }</h3>
 
